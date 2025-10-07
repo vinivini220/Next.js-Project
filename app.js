fb9f12425a35b85558faefa38f3c2fa5bebff3dd
vinivini220 <vinivini1688@gmail.com>
2025-10-07 11:07:28 +0530

    commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express=require('express')
+const app=express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to EasyShop")
+})
+app.listen(PORT,()=>
+{
+    console.log(`Server is running on port ${PORT}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
